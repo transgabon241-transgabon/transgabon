7cde505261ee769fc4f5b027f72dbf74e8f3e1f6
EKO ASSOUMOU Jean de Dieu <ekojdd@gmail.com>
2026-07-20 09:05:46 +0100

    mis à jour

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -88,7 +88,7 @@ export default function HomePage() {
         <div className="container mx-auto px-4 py-16 md:py-24 relative z-10">
           <div className="max-w-2xl mx-auto text-center mb-10">
             <h1 className="text-3xl md:text-5xl font-semibold mb-4 leading-tight tracking-tight">
-              Voyagez - Expédiez et Suivez vos expéditions à travers tout le Gabpon
+              Voyagez et expédiez vos colis à travers le Gabon en toute simplicité.
             </h1>
             <p className="text-lg md:text-xl opacity-90 leading-relaxed font-medium">
               TransGabon-Connect simplifie vos déplacements et l'envoi de vos colis à travers tout le Gabon. Réservez vos billets, expédiez vos colis et suivez leur acheminement en temps réel, le tout depuis une seule application.
